bb24f42c836ba0547e9afc31520b25d910bb8213
Graeme Stewart <gi_stewart@yahoo.com>
2020-05-04 21:04:45 -0400

    test of API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,13 +3,19 @@ var db = require("../models");
 //Get Earnings
 module.exports = function (app) {
   app.get("/api/poolsters", function (req, res) {
-    db.Poolsters.findAll({}).then((data) => {
+    db.poolsters.findAll({}).then((data) => {
       res.json(data);
     });
   });
 
   app.get("/api/schedule", function (req, res) {
     db.Schedule.findAll({}).then((data) => {
+      res.json(data);
+    });
+  });
+
+  app.get("/api/results", function (req, res) {
+    db.Results.findAll({}).then((data) => {
       res.json(data);
     });
   });
