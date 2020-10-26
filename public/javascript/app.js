cec7e67756ce343bae6775156dee62f967a6b211
Graeme Stewart <gi_stewart@yahoo.com>
2020-10-26 11:16:01 -0400

    for loading of purse values only

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     await eventCheck();
     await missingResults();
     lastEventDetails();
-    await displayLiveTab();
+    // await displayLiveTab();
     setTimeout(function () {
       // seasonData();
       liveEvent();
